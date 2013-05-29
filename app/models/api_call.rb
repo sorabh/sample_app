7c653b345b68a520d10743e13cd07dd90095ec13
sorabh <audace.sourabh@gmail.com>
2013-05-29 14:44:12 +0530

    https

--- app/models/api_call.rb
+++ app/models/api_call.rb
@@ -6,8 +6,6 @@ class ApiCall < ActiveRecord::Base
   def make_api_call(call)
     doctor=User.find(call.doctor_id)
     url ='https://gds.eligibleapi.com/v1/plan/all.json?'+'api_key=Test' +'&payer_id='+(call.payer_id).to_s+'&payer_name='+call.payer_name.to_s+'&subscriber_dob='+call.subscriber_dob.to_s+'&subscriber_id='+call.subscriber_id.to_s+'&subscriber_last_name='+call.subscriber_last_name.to_s+'&subscriber_first_name='+call.subscriber_first_name.to_s+'&service_provider_last_name='+doctor.l_name.to_s+'&service_provider_first_name='+doctor.f_name.to_s+'&service_provider_NPI='+doctor.npi.to_s
-    http = Net::HTTP.new
-    http.use_ssl = true
-    call.responce = http.get(URI.parse(url),nil)
+    call.responce = Net::HTTP.get(URI.parse(url))
   end
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,5 @@
 EligibleapiSampleApp::Application.configure do
+  config.force_ssl = true
   # Settings specified here will take precedence over those in config/application.rb
 
   # Code is not reloaded between requests
