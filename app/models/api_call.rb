a3ed53f7d26327b3fe397448e228abaf0d95a1ec
sorabh <audace.sourabh@gmail.com>
2013-05-29 14:25:35 +0530

    https

--- app/models/api_call.rb
+++ app/models/api_call.rb
@@ -5,8 +5,9 @@ class ApiCall < ActiveRecord::Base
   validate :doctor_id, :patient_contact_no, :payer_id, :payer_name, :subscriber_dob, :subscriber_first_name, :subscriber_id, :subscriber_last_name, :presence => true
   def make_api_call(call)
     doctor=User.find(call.doctor_id)
-    http.use_ssl = true
     url ='https://gds.eligibleapi.com/v1/plan/all.json?'+'api_key=Test' +'&payer_id='+(call.payer_id).to_s+'&payer_name='+call.payer_name.to_s+'&subscriber_dob='+call.subscriber_dob.to_s+'&subscriber_id='+call.subscriber_id.to_s+'&subscriber_last_name='+call.subscriber_last_name.to_s+'&subscriber_first_name='+call.subscriber_first_name.to_s+'&service_provider_last_name='+doctor.l_name.to_s+'&service_provider_first_name='+doctor.f_name.to_s+'&service_provider_NPI='+doctor.npi.to_s
-    call.responce = Net::HTTP.get(URI.parse(url))
+    http = Net::HTTP.new()
+    http.use_ssl = true
+    call.responce = http.URI.parse(url)
   end
 end
