5b84371a70d475a3f0aef922ed3909ae23ce24ab
sorabh <audace.sourabh@gmail.com>
2013-05-29 19:05:36 +0530

    adding use_ssl

--- app/models/api_call.rb
+++ app/models/api_call.rb
@@ -7,7 +7,9 @@ class ApiCall < ActiveRecord::Base
     uri =URI('https://gds.eligibleapi.com/v1/plan/all.json?'+'api_key=Test' +'&payer_id='+(call.payer_id).to_s+'&payer_name='+call.payer_name.to_s+'&subscriber_dob='+call.subscriber_dob.to_s+'&subscriber_id='+call.subscriber_id.to_s+'&subscriber_last_name='+call.subscriber_last_name.to_s+'&subscriber_first_name='+call.subscriber_first_name.to_s+'&service_provider_last_name='+doctor.l_name.to_s+'&service_provider_first_name='+doctor.f_name.to_s+'&service_provider_NPI='+doctor.npi.to_s)
    # call.responce= Net::HTTP.get(URI.parse(url))
     Net::HTTP.start(uri.host, uri.port,
-                    :use_ssl => uri.scheme == 'https') do |http|
+                    :use_ssl => uri.scheme == 'https',:verify_mode => OpenSSL::SSL::VERIFY_NONE
+    ) do |http|
+
       request = Net::HTTP::Get.new uri
 
       responce_object = http.request request # Net::HTTPResponse object
