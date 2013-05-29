0adc574c8e7027320856cf38cc6b4281623f138b
sorabh <audace.sourabh@gmail.com>
2013-05-29 19:24:25 +0530

    modifing ssl

--- app/models/api_call.rb
+++ app/models/api_call.rb
@@ -6,15 +6,14 @@ class ApiCall < ActiveRecord::Base
     doctor=User.find(call.doctor_id)
     uri =URI.parse('https://gds.eligibleapi.com/v1/plan/all.json?'+'api_key=Test' +'&payer_id='+(call.payer_id).to_s+'&payer_name='+call.payer_name.to_s+'&subscriber_dob='+call.subscriber_dob.to_s+'&subscriber_id='+call.subscriber_id.to_s+'&subscriber_last_name='+call.subscriber_last_name.to_s+'&subscriber_first_name='+call.subscriber_first_name.to_s+'&service_provider_last_name='+doctor.l_name.to_s+'&service_provider_first_name='+doctor.f_name.to_s+'&service_provider_NPI='+doctor.npi.to_s)
    # call.responce= Net::HTTP.get(URI.parse(url))
-    Net::HTTP.start(uri.host, uri.port,
-                    :use_ssl => uri.scheme == 'https',:verify_mode => OpenSSL::SSL::VERIFY_NONE
-    ) do |http|
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
 
-      request = Net::HTTP::Get.new uri
+    request = Net::HTTP::Get.new(uri.request_uri)
 
-      responce_object = http.request request # Net::HTTPResponse object
-      call.responce = responce_object.body
-    end
+    response = http.request(request)
+    call.responce=response.body
 
 
 
