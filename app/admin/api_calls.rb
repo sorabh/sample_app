d4add76f5172f3696a1b8a7de3155794535a99d0
sorabh <audace.sourabh@gmail.com>
2013-05-31 11:52:17 +0530

    active admin api calls

---
+++ app/admin/api_calls.rb
@@ -0,0 +1,13 @@
+ActiveAdmin.register ApiCall do
+  config.per_page = 10
+  index do
+    column "Doctor id",:doctor_id
+    column :payer_id
+    column :payer_name
+    column :subscriber_first_name
+    column :subscriber_last_name
+    column :subscriber_id
+    column :patient_contact_no
+
+  end
+end
